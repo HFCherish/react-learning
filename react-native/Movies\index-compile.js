d0b2bf1db62cebbedd2023221bea7952f6d638d2
HFCherish <hf_cherish@163.com>
2018-05-16 19:00:07 +0800

    adjust directory

--- react-native/Movies\index-compile.js
+++ react-native/Movies\index-compile.js
@@ -1,3 +1,3 @@
-var _reactNative=require('react-native');var _App=require('./App');var _App2=_interopRequireDefault(_App);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}_reactNative.AppRegistry.registerComponent('Movies',function(){return _App2.default;});
+var _reactNative=require('react-native');var _App=require('./src/App');var _App2=_interopRequireDefault(_App);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}_reactNative.AppRegistry.registerComponent('Movies',function(){return _App2.default;});
 
 //# sourceMappingURL=Movies\index-compile.js.map
