589c98d43fbb5d47fde7984f3a1950dc0cee253b
HFCherish <hf_cherish@163.com>
2018-05-23 17:37:13 +0800

    init movie again

--- react-native/Movies/src/App.js
+++ react-native/Movies/src/App.js
@@ -4,122 +4,58 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import ServerClient from './ServerClient';
-
+import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View,
-  FlatList,
-  Image
+  View
 } from 'react-native';
-
-const MOCKED_MOVIES = [
-  {
-    title: 'Flipped',
-    year: 2010,
-    posters: {
-      thumbnail: 'http://i.imgur.com/UePbdph.jpg'
-    }
-  }
-];
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-  'Cmd+D or shake for dev menu',
+    'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-  'Shake or press menu button for dev menu',
+    'Shake or press menu button for dev menu',
 });
 
-type
-  Props = {};
+type Props = {};
 export default class App extends Component<Props> {
-  state = {
-    data: [],
-    loaded: false
-  };
-
-  componentDidMount() {
-    ServerClient.getMovies(json => {
-      this.setState({
-        data: this.state.data.concat(json.movies),
-        loaded: true
-      });
-    });
-  }
-
-  renderLoadingView() {
+  render() {
     return (
       <View style={styles.container}>
-        <Text>正在加载电影数据...</Text>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+        <Text style={styles.instructions}>
+          haha from petrina
+        </Text>
       </View>
     );
   }
-
-  render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
-    return (
-      <View style={styles.container}>
-        {/*这里 renderItem 中必须用 item，这其实是指定 renderItem 中的属性 item，有两个 item、index*/}
-        <FlatList style={styles.movieList} data={this.state.data} renderItem={ ({item: movie}) => <Movie movie={movie}/>} />
-      </View>
-    );
-  }
-}
-
-class Movie extends Component {
-  render() {
-    let movie = this.props.movie;
-    return (
-      <View style={styles.movieContainer}>
-        <Image source={{uri: movie.posters.thumbnail}}
-               style={styles.thumbnail}/>
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
-      </View>
-    );
-  }
-
 }
 
 const styles = StyleSheet.create({
-  movieList: {
-    paddingTop: 20
-  },
   container: {
-    flex: 1
-  },
-  movieContainer: {
     flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  rightContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  thumbnail: {
-    width: 53,
-    height: 81
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
-  title: {
-    fontSize: 16,
-    marginBottom: 10
-  },
-  year: {
-    fontSize: 10,
-    color: '#404956'
-  }
-
 });
