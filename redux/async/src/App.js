6b2173c69fae40b1659c5b3b5bc9d4cc9b6a019f
HFCherish <hf_cherish@163.com>
2018-06-01 16:46:50 +0800

    static componet framework

--- redux/async/src/App.js
+++ redux/async/src/App.js
@@ -1,15 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+const Picker = ({subreddit, onSelect, options}) => (
+  <span>
+    <h1>{subreddit}</h1>
+    <select onChange={event => onSelect(event.target.value)}>
+      {
+        options.map(option => (
+          <option value={option}>{option}</option>
+        ))
+      }
+    </select>
+  </span>
+);
+
+const Posts = ({posts}) => (
+  <ul>
+    {
+      posts.map(post => (
+        <li>{post.title}</li>
+      ))
+    }
+  </ul>
+);
 
 class App extends Component {
+  state = {
+    subreddit: 'reactjs',
+    posts: [
+      {title: 'Beginner\'s Thread / Easy Question (May 2018)'},
+      {title: 'Test Driven Development (TDD) in React &amp; React Native'},
+      {title: 'The most important lessons I’ve learned after a year of working with React'}
+    ],
+    lastUpdated: new Date()
+  };
+
+  onSelect = (selectedSubreddit) => {
+    this.setState({
+      ...this.state,
+      subreddit: selectedSubreddit
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <Picker subreddit={this.state.subreddit} onSelect={this.onSelect} options={['reactjs', 'frontend']} />
+        <p>
+          <span>last updated at: {this.state.lastUpdated.toLocaleTimeString()}. {' '}</span>
+          <button>refresh</button>
         </p>
+        <Posts posts={this.state.posts} />
       </div>
     );
   }
